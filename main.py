1f244b32fc87d51f3828fef6206e7e2227e34b15
ovokpus <ovo@live.ca>
2022-04-02 05:26:17 -0600

    implemented logging

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import logging
+
 from fastapi import FastAPI
+from opencensus.ext.azure.log_exporter import AzureLogHandler
 from pydantic import BaseModel
-import time
-import asyncio
+
 import utils
 
 app = FastAPI()
+
+logger = logging.getLogger(__name__)
+logger.setLevel(10)
+logger.addHandler(AzureLogHandler(
+    connection_string='InstrumentationKey=ffe3b82f-1430-4ebb-ab39-34aa9a55f86b'))
 
 headers = {
     "Ocp-Apim-Subscription-Key": '7b27544096db4f64a5d9e64963303ab0',
@@ -21,6 +28,7 @@ class Model(BaseModel):
 def analyze_text(text: Model):
     response = {"sentiment": [], "keyphrases": []}
     no_of_text = len(text.text_to_analyze)
+    tasks = []
     for i in range(no_of_text):
         document = {"documents": [
             {"id": i + 1, "language": "en", "text": text.text_to_analyze[i]}]}
@@ -29,6 +37,16 @@ def analyze_text(text: Model):
             headers, document, endpoint='sentiment')
         keyphrases = utils.call_text_analytics_api(
             headers, document, endpoint='keyPhrases')
+
+        log_data = {
+            "custom_dimensions":
+            {
+                "text": text.text_to_analyze[i],
+                "text_sentiment": sentiment["documents"][0]["sentiment"],
+                "text_keyphrases": keyphrases["documents"][0]["keyPhrases"]
+            }
+        }
+        logger.info('Text Processed Successfully', extra=log_data)
 
         response["sentiment"].append(
             sentiment["documents"][0])
